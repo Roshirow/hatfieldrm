037909352b11dae1e3745e2507d2626c2171c61e
Rochell <44246623+Roshirow@users.noreply.github.com>
2020-08-25 17:37:05 -0700

    fixed project pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
-
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    links.forEach(link => {
-        link.classList.toggle('fade');
-    });
-});
-
 // LIGHT GALLERY
 $('#gallery-1').lightGallery({
     selector: 'a',
@@ -20,5 +8,3 @@ require(['./lightgallery.js'], function() {
         $("#lightgallery").lightGallery(); 
     });
 });
-
-alert("Please note that this website is under construction");
