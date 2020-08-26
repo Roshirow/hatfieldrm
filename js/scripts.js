9eb3b7e6f522feb08657e85587908c9744e30e3a
Rochell <44246623+Roshirow@users.noreply.github.com>
2020-08-25 23:29:52 -0700

    cats

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ require(['./lightgallery.js'], function() {
         $("#lightgallery").lightGallery(); 
     });
 });
+
+var getStuff = 'https://api.thecatapi.com/v1/images/search';
+
+var setStuff = {};
+
+var doStuff = function(data){
+  
+ showStuff (data);
+  
+};
+
+$.getJSON( getStuff, setStuff, doStuff);
+
+var showStuff = function(data) {console.log(data[0].url);
+                                
+                                $('img').attr( 'src' , data[0].url  );
+                                
+       }
